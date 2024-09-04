b61ad2789de728980c823d701ce0c91783006348
Anders Englöf Ytterström <anders@playmaker.ai>
2024-09-04 13:01:30 +0200

    Add Req dependency

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Mse25.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:req, "~> 0.5.0"}
     ]
   end
 
