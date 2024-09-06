00ec341bd1070b32caf4c860520c5cd05c8c0178
Anders Englöf Ytterström <yttan@fastmail.se>
2024-09-06 15:07:05 +0200

    Add item controller as pokemon route

--- lib/mse25_web/router.ex
+++ lib/mse25_web/router.ex
@@ -18,6 +18,7 @@ defmodule Mse25Web.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    get "/*path", ItemController, :index
   end
 
   # Other scopes may use custom stacks.
