36657a6ec74b147662b2b6c606c8c612e2363350
Anders Englöf Ytterström <yttan@fastmail.se>
2024-10-03 21:50:55 +0200

    Show band information on festival-ish events

--- lib/mse25_web/controllers/page_html.ex
+++ lib/mse25_web/controllers/page_html.ex
@@ -14,4 +14,29 @@ defmodule Mse25Web.PageHTML do
     |> Enum.join(", ")
     |> String.replace(~r/, ([^,]+?)$/, " och \\1")
   end
+
+  def hilights?(%{"bands" => bands, "category" => category}) do
+    _festival_band?(bands, category)
+  end
+
+  def missed?(%{"mia" => bands, "category" => category}) do
+    _festival_band?(bands, category)
+  end
+
+  def opening_acts?(%{"bands" => [_ | bands], "category" => "concert"}) do
+    not Enum.empty?(bands)
+  end
+
+  def opening_acts?(_) do
+    false
+  end
+
+  defp _festival_band?(bands, category)
+       when category in ["cruise", "openairfestival", "cityfestival", "onedayfestival"] do
+    not Enum.empty?(bands)
+  end
+
+  defp _festival_band?(_b, _c) do
+    false
+  end
 end
