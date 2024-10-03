1a9399354276887454ca1750cf7e8ffa094062e1
Anders Englöf Ytterström <yttan@fastmail.se>
2024-10-03 22:36:36 +0200

    Extract view helpers to reusable module
    Code is shared between ItemHTML and PageHTML,
    and they really do not belong in web anyway
    since it is usable outside web scope as well.

--- lib/mse25_web/controllers/item_html.ex
+++ lib/mse25_web/controllers/item_html.ex
@@ -1,5 +1,7 @@
 defmodule Mse25Web.ItemHTML do
   use Mse25Web, :html
 
+  import Mse25.Utils
+
   embed_templates "item_html/*"
 end
--- lib/mse25_web/controllers/page_html.ex
+++ lib/mse25_web/controllers/page_html.ex
@@ -5,38 +5,7 @@ defmodule Mse25Web.PageHTML do
   See the `page_html` directory for all templates available.
   """
   use Mse25Web, :html
+  import Mse25.Utils
 
   embed_templates "page_html/*"
-
-  def bandlist(bands) do
-    bands
-    |> Enum.map(fn b -> b["artists_id"]["name"] end)
-    |> Enum.join(", ")
-    |> String.replace(~r/, ([^,]+?)$/, " och \\1")
-  end
-
-  def hilights?(%{"bands" => bands, "category" => category}) do
-    _festival_band?(bands, category)
-  end
-
-  def missed?(%{"mia" => bands, "category" => category}) do
-    _festival_band?(bands, category)
-  end
-
-  def opening_acts?(%{"bands" => [_ | bands], "category" => "concert"}) do
-    not Enum.empty?(bands)
-  end
-
-  def opening_acts?(_) do
-    false
-  end
-
-  defp _festival_band?(bands, category)
-       when category in ["cruise", "openairfestival", "cityfestival", "onedayfestival"] do
-    not Enum.empty?(bands)
-  end
-
-  defp _festival_band?(_b, _c) do
-    false
-  end
 end
