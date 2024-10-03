92844022503b2eee87a0c2b694ce56158d94d217
Anders Englöf Ytterström <yttan@fastmail.se>
2024-10-03 14:36:31 +0200

    Let user copy link permalink to clipboard
    if alt, shift or ctrl are pressed, fallback to
    default behavior.

--- assets/js/copy-to-clipboard.js
+++ assets/js/copy-to-clipboard.js
@@ -1,14 +1,30 @@
-export default () => {
+export const copyCodeToClipboard = () => {
   const codeblocks = document.querySelectorAll("pre>code");
-
   for (const b of codeblocks) {
     const button = document.createElement("button");
     button.innerHTML = "Kopiera";
-    button.addEventListener("click", function ({target}) {
+    button.addEventListener("click", function ({ target }) {
       const text = target.previousSibling.innerHTML;
       navigator.clipboard.writeText(text);
-    })
+    });
     b.parentNode.appendChild(button);
   }
-}
+};
 
+export const copyUrlToClipboard = () => {
+  const permalinks = document.querySelectorAll(".permalink");
+  for (const pl of permalinks) {
+    pl.setAttribute(
+      "title",
+      pl.getAttribute("title") + ", klicka för att kopiera",
+    );
+    pl.addEventListener("click", function (evt) {
+      const { target, shiftKey, ctrlKey, altKey } = evt;
+      if (!shiftKey && !ctrlKey && !altKey) {
+        evt.preventDefault();
+        const text = target.href;
+        navigator.clipboard.writeText(text);
+      }
+    });
+  }
+};
