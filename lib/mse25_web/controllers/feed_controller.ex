405567de3b7ef255a834c7c84376762738b946e8
Anders Englöf Ytterström <yttan@fastmail.se>
2024-10-07 09:10:17 +0200

    Add event json to Feed Controller

--- lib/mse25_web/controllers/feed_controller.ex
+++ lib/mse25_web/controllers/feed_controller.ex
@@ -41,8 +41,36 @@ defmodule Mse25Web.FeedController do
     )
   end
 
-  def events_json() do
-    :tbw
+  def events_json(conn, _) do
+    json(
+      conn,
+      Directus.get_events!(limit: 9999)
+      |> Enum.map(fn %{
+                       "title" => title,
+                       "lead" => lead,
+                       "poster" => img,
+                       "started_at" => date,
+                       "location" => %{
+                         "name" => venue,
+                         "address" => region,
+                         "position" => %{
+                           "coordinates" => [lat, lng]
+                         }
+                       },
+                       "bands" => bands
+                     } ->
+        %{
+          title: title,
+          lead: lead,
+          img: img,
+          date: String.slice(date, 0..9),
+          region: region,
+          venue: venue,
+          location: [lng, lat],
+          bands: Enum.map(bands, fn %{"artists_id" => %{"name" => band}} -> band end)
+        }
+      end)
+    )
   end
 
   def event_map_js() do
